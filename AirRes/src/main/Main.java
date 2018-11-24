d50624722c2e4f9baf4c2fc7c13bdbc65da4bfff
zach <zach@zach-MacBookPro>
2018-11-24 17:46:54 -0500

    Added Book Flights Button

--- AirRes/src/main/Main.java
+++ AirRes/src/main/Main.java
@@ -68,15 +68,15 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{306, 388, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{43, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{43, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblNewLabel = new JLabel("Airline Booking System");
 		lblNewLabel.setFont(new Font("Ubuntu", Font.PLAIN, 37));
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.insets = new Insets(2, 2, 5, 5);
 		gbc_lblNewLabel.anchor = GridBagConstraints.NORTHWEST;
 		gbc_lblNewLabel.gridx = 1;
 		gbc_lblNewLabel.gridy = 0;
@@ -90,10 +90,16 @@ public class Main {
 				
 			}
 		});
+		
+		JButton btnNewButton_2 = new JButton("Book Flights");
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.insets = new Insets(2, 2, 5, 5);
+		gbc_btnNewButton_2.gridx = 1;
+		gbc_btnNewButton_2.gridy = 4;
+		frame.getContentPane().add(btnNewButton_2, gbc_btnNewButton_2);
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton_1.gridx = 3;
-		gbc_btnNewButton_1.gridy = 3;
+		gbc_btnNewButton_1.gridy = 4;
 		frame.getContentPane().add(btnNewButton_1, gbc_btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("Choose Passenger File");
@@ -105,9 +111,9 @@ public class Main {
 			}
 		});
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.insets = new Insets(2, 2, 0, 5);
 		gbc_btnNewButton.gridx = 0;
-		gbc_btnNewButton.gridy = 3;
+		gbc_btnNewButton.gridy = 4;
 		frame.getContentPane().add(btnNewButton, gbc_btnNewButton);
 	}
 
